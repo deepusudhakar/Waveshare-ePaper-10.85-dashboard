16f388129424dc284a956f0a07d9a815ddf96423
Deepu Sudhakar <deepu@chillpad.io>
2026-06-29 15:56:33 -0500

    main: shorten full-refresh interval to 120 cycles (~2hr) to limit ghosting

--- main.py
+++ main.py
@@ -1226,7 +1226,7 @@ def main():
                 image = render_screen(epd, fonts)
                 buf = epd.getbuffer(image)
 
-                if refresh_counter >= 600:
+                if refresh_counter >= 120:
                     logging.info("Full Refresh cycle")
                     epd.init()
                     epd.display(buf)
